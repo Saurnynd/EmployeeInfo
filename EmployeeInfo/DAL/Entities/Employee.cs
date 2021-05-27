3e5d7f172d5c688bdee2618624afb1cc6d8e565b
Saurnynd <ballekus7@gmail.com>
2021-05-27 04:41:58 +0300

    Added Contracts of Repositories

---
+++ EmployeeInfo/DAL/Entities/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    class Employee
+    {
+        public int EmployeeID { get; set; }
+        public int Position { get; set; }
+    }
+}
